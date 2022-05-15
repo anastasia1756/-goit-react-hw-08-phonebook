df13e19735e69d53e952a41ebda2e85baccc38e6
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-05-15 03:54:03 +0200

    Updates

---
+++ static/js/767.a39165c6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[767],{767:function(n,o,e){e.r(o),e.d(o,{Btn:function(){return u}});var r,t=e(168),c=e(4835),u=(0,e(955).ZP)(c.Z)(r||(r=(0,t.Z)(["\n  background-color: ",";\n\n  &:hover {\n    background-color: ",";\n  }\n"])),(function(n){return n.theme.colors.metalic}),(function(n){return n.theme.colors.newspapper}))}}]);
+//# sourceMappingURL=767.a39165c6.chunk.js.map
